f8cd55789d1d5f1de6843725a4cf2527f0b95515
Imamhassan <imamhsn195@gmail.com>
2024-02-05 18:31:23 +0400

    code refactor

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,28 +39,30 @@ const updateUser = async (req, res) => {
     const userId = req.params.id;
 
     const profile_picture = req.file.destination.substr(7) + req.file.filename;
-
-    const afterUpdateUser = {'profile_picture': profile_picture, ...req.body};
     
+    const afterUpdateUser = { 'profile_picture': profile_picture, ...req.body };
+
     try {
         const beforeUpdateUser = await User.findById(userId);
 
+        if (!beforeUpdateUser) {
+            return res.status(404).json({ error: "User is not found." });
+        }
+
         const old_profile_picture = beforeUpdateUser.profile_picture;
 
-        const updatedUser = await User.findByIdAndUpdate(userId, afterUpdateUser , { new: true })
+        const updatedUser = await User.findByIdAndUpdate(userId, afterUpdateUser, { new: true });
 
-        if(!updatedUser){
-
-            res.status(404).json({error: "User is not found."});
-
-        }else{
-
-            fs.unlink(`public/${old_profile_picture}`, (error) => { console.log(error); });
-            
-            res.status(200).json({ message: "User is updated successfully.", data: updatedUser })
+        if (!updatedUser) {
+            return res.status(404).json({ error: "User is not found." });
         }
+
+        await fs.promises.unlink(`public/${old_profile_picture}`);
+        
+        res.status(200).json({ message: "User is updated successfully.", data: updatedUser });
     } catch (error) {
-        res.status(500).json({error: "Internal Server error"})
+        console.error(error);
+        res.status(500).json({ error: "Internal Server error" });
     }
 }
 
